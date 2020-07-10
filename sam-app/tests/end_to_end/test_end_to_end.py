530fb8f3abb16a2bf93b8cb1156ca88fd8d4413f
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-07-10 08:35:59 -0400

    update end-to-end tests and template for dynamic s3 name

--- sam-app/tests/end_to_end/test_end_to_end.py
+++ sam-app/tests/end_to_end/test_end_to_end.py
@@ -1,5 +1,6 @@
 import unittest
 import boto3
+import json
 from datetime import datetime
 from unittest.mock import patch, Mock, MagicMock, PropertyMock
 
@@ -12,17 +13,34 @@ def create_s3_text_file(bucket, key, file_text):
     return response
 
 
+def get_s3_bucket_from_stack(output_key):
+    cloudformation = boto3.client("cloudformation")
+    stacks = cloudformation.describe_stacks(StackName="lutils")
+    stack_outputs = stacks["Stacks"][0]["Outputs"]
+    s3_bucket = ""
+    for output in stack_outputs:
+        if output["OutputKey"] == output_key:
+            s3_bucket = output["OutputValue"]
+            break
+    return s3_bucket.split(":")[-1:][0]
+
+
 class EndToEndTests(unittest.TestCase):
     def test_upload_new_starting_file__given_upload_new_starting_file__then_new_html_pages_downloaded(
         self,
     ):
         # Arrange
-        list = """https://www.twitter.com
-https://www.facebook.com
-https://instagram.com"""
+        cf = boto3.client("cloudformation")
+        stack = cf.describe_stacks(StackName="lutils")
+        # print(json.dumps(stack, indent=3, default=str))
+        bucket = get_s3_bucket_from_stack("S3ProcessingBucket")
+        print(f"*** {bucket}")
+
+        list = """https://www.icf.com
+https://usa.gov"""
         now = datetime.now().isoformat().replace(":", "")
         create_s3_text_file(
-            "lutils-processing",
+            bucket,
             f"lutil_s3_text_lines_to_sns/lutil_s3_text_lines_output/test_{now}.txt",
             list,
         )
