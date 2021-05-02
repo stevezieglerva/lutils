4c3c8fc68ba26aaf04e84b6e1f1b6f2ab0703a07
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-05-02 12:21:51 -0400

    add sns filter

--- sam-app/tests/integration/test_int_lutil_fan_handler.py
+++ sam-app/tests/integration/test_int_lutil_fan_handler.py
@@ -195,6 +195,108 @@ EVENT_INSERT_INVALID_SNS_ARN = {
 }
 
 
+EVENT_INSERT_3 = {
+    "Records": [
+        {
+            "eventID": "080ff8a233fb69e74ef8f1130f0cfa00",
+            "eventName": "INSERT",
+            "eventVersion": "1.1",
+            "eventSource": "aws:dynamodb",
+            "awsRegion": "us-east-1",
+            "dynamodb": {
+                "ApproximateCreationDateTime": 1619955850.0,
+                "Keys": {
+                    "pk": {
+                        "S": "FAN-OUT-JOB#b530ebf0-ab3b-11eb-b19b-acde48001122-TASK#task C"
+                    }
+                },
+                "NewImage": {
+                    "task_name": {"S": "task C"},
+                    "process_id": {"S": "b530ebf0-ab3b-11eb-b19b-acde48001122"},
+                    "process_name": {"S": "processA"},
+                    "pk": {
+                        "S": "FAN-OUT-JOB#b530ebf0-ab3b-11eb-b19b-acde48001122-TASK#task C"
+                    },
+                    "message": {"S": '{\n   "keywords": "hello world"\n}'},
+                    "completion_sns_arn": {"S": "sns-arn"},
+                    "status": {"S": "created"},
+                    "status_change_timestamp": {"S": "2021-05-02T07:44:09.627753"},
+                    "timestamp": {"S": "2021-05-02T07:44:09.627604"},
+                },
+                "SequenceNumber": "3774300000000008979127455",
+                "SizeBytes": 366,
+                "StreamViewType": "NEW_AND_OLD_IMAGES",
+            },
+            "eventSourceARN": "arn:aws:dynamodb:us-east-1:112280397275:table/lutils-FanProcessingTableTest-X541MIGMFYBW/stream/2021-05-01T16:26:21.720",
+        },
+        {
+            "eventID": "080ff8a233fb69e74ef8f1130f0cfa00",
+            "eventName": "INSERT",
+            "eventVersion": "1.1",
+            "eventSource": "aws:dynamodb",
+            "awsRegion": "us-east-1",
+            "dynamodb": {
+                "ApproximateCreationDateTime": 1619955850.0,
+                "Keys": {
+                    "pk": {
+                        "S": "FAN-OUT-JOB#b530ebf0-ab3b-11eb-b19b-acde48001122-TASK#task C"
+                    }
+                },
+                "NewImage": {
+                    "task_name": {"S": "task C"},
+                    "process_id": {"S": "b530ebf0-ab3b-11eb-b19b-acde48001122"},
+                    "process_name": {"S": "processB"},
+                    "pk": {
+                        "S": "FAN-OUT-JOB#b530ebf0-ab3b-11eb-b19b-acde48001122-TASK#task C"
+                    },
+                    "message": {"S": '{\n   "keywords": "hello world"\n}'},
+                    "completion_sns_arn": {"S": "sns-arn"},
+                    "status": {"S": "created"},
+                    "status_change_timestamp": {"S": "2021-05-02T07:44:09.627753"},
+                    "timestamp": {"S": "2021-05-02T07:44:09.627604"},
+                },
+                "SequenceNumber": "3774300000000008979127455",
+                "SizeBytes": 366,
+                "StreamViewType": "NEW_AND_OLD_IMAGES",
+            },
+            "eventSourceARN": "arn:aws:dynamodb:us-east-1:112280397275:table/lutils-FanProcessingTableTest-X541MIGMFYBW/stream/2021-05-01T16:26:21.720",
+        },
+        {
+            "eventID": "080ff8a233fb69e74ef8f1130f0cfa00",
+            "eventName": "INSERT",
+            "eventVersion": "1.1",
+            "eventSource": "aws:dynamodb",
+            "awsRegion": "us-east-1",
+            "dynamodb": {
+                "ApproximateCreationDateTime": 1619955850.0,
+                "Keys": {
+                    "pk": {
+                        "S": "FAN-OUT-JOB#b530ebf0-ab3b-11eb-b19b-acde48001122-TASK#task C"
+                    }
+                },
+                "NewImage": {
+                    "task_name": {"S": "task C"},
+                    "process_id": {"S": "b530ebf0-ab3b-11eb-b19b-acde48001122"},
+                    "process_name": {"S": "processC"},
+                    "pk": {
+                        "S": "FAN-OUT-JOB#b530ebf0-ab3b-11eb-b19b-acde48001122-TASK#task C"
+                    },
+                    "message": {"S": '{\n   "keywords": "hello world"\n}'},
+                    "completion_sns_arn": {"S": "sns-arn"},
+                    "status": {"S": "created"},
+                    "status_change_timestamp": {"S": "2021-05-02T07:44:09.627753"},
+                    "timestamp": {"S": "2021-05-02T07:44:09.627604"},
+                },
+                "SequenceNumber": "3774300000000008979127455",
+                "SizeBytes": 366,
+                "StreamViewType": "NEW_AND_OLD_IMAGES",
+            },
+            "eventSourceARN": "arn:aws:dynamodb:us-east-1:112280397275:table/lutils-FanProcessingTableTest-X541MIGMFYBW/stream/2021-05-01T16:26:21.720",
+        },
+    ]
+}
+
+
 def get_sns_arn_from_stack(output_key):
     cloudformation = boto3.client("cloudformation")
     stacks = cloudformation.describe_stacks(StackName="lutils")
@@ -217,4 +319,15 @@ class FanHandlerIntTests(unittest.TestCase):
 
         # Assert
         expected = {"inserted": {"processA": 1}}
+        self.assertEqual(results, expected)
+
+    def test_lambda_handler__given_three_insert_events__then_three_sns_sent(self):
+        # Arrange
+        os.environ["HANDLER_SNS_TOPIC_ARN"] = get_sns_arn_from_stack("FanEventsTestSNS")
+
+        # Act
+        results = app.lambda_handler(EVENT_INSERT_3, {})
+
+        # Assert
+        expected = {"inserted": {"processA": 1, "processB": 1, "processC": 1}}
         self.assertEqual(results, expected)
