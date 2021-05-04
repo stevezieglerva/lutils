ae7046f3ff5db03f485f72eea32f844a092817b3
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-05-04 18:48:30 -0400

    update producer to use the new publishers

--- sam-app/lutil_fan_e2e_producer/app.py
+++ sam-app/lutil_fan_e2e_producer/app.py
@@ -7,18 +7,18 @@ import json
 import sys
 import glob
 
-from FanOut import FanOut
+from FanEventPublisher import FanEventPublisher
 
 
 def lambda_handler(event, context):
     print(f"Started at {datetime.now()}")
 
-    fan = FanOut("e2e test", "sns-done", "lutils-FanProcessingTableTest-X541MIGMFYBW")
+    publisher = FanEventPublisher(os.environ["HANDLER_SNS_TOPIC_ARN"])
+
     # Create a group of fan out events for the "e2e test" process
     for i in range(10):
-        new_job = fan.fan_out(
-            f"task #{i}", {"number": i, "parameters": f"38jdjsls-{i}"}
-        )
+        value = i * 33
+        publisher.fan_out("e2e tests", f"task-{i}", {"var_1": value})
 
     print(f"Finished at {datetime.now()}")
 
