52a45845a3d642f28ace64c8b9483a1e09ca6fce
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-05-05 09:41:42 -0400

    update publish

--- sam-app/lutil_fan_e2e_producer/app.py
+++ sam-app/lutil_fan_e2e_producer/app.py
@@ -14,13 +14,12 @@ def lambda_handler(event, context):
     print(f"Started at {datetime.now()}")
 
     publisher = FanEventPublisher(os.environ["HANDLER_SNS_TOPIC_ARN"])
+    process_id = publisher.generate_process_id()
 
     # Create a group of fan out events for the "e2e test" process
     for i in range(10):
         value = i * 33
-        publisher.fan_out(
-            publisher.generate_process_id(), "e2e tests", f"task-{i}", {"var_1": value}
-        )
+        publisher.fan_out(process_id, "e2e tests", f"task-{i}", {"var_1": value})
 
     print(f"Finished at {datetime.now()}")
 
