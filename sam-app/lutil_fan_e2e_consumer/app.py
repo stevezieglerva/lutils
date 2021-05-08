4a4a60167de1271fc2d6ce61c90ee9fefd112669
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-05-07 21:10:44 -0400

    add max delay

--- sam-app/lutil_fan_e2e_consumer/app.py
+++ sam-app/lutil_fan_e2e_consumer/app.py
@@ -18,6 +18,8 @@ import FanEventOptions
 
 def lambda_handler(event, context):
     print(f"Started at {datetime.now()}")
+    max_delay = event.get("max_delay", 5)
+
     print(json.dumps(event, indent=3, default=str))
     publisher = FanEventPublisher(os.environ["HANDLER_SNS_TOPIC_ARN"])
 
@@ -31,7 +33,7 @@ def lambda_handler(event, context):
         task = TaskRecord(record_string=json.dumps(event.message, default=str))
         publisher.publish_event(event.event_source, TASK_STARTED, task.json())
 
-        time.sleep(random.randint(0, 5))
+        time.sleep(random.randint(0, max_delay))
 
         publisher.publish_event(event.event_source, TASK_COMPLETED, task.json())
     print(f"Finished at {datetime.now()}")
