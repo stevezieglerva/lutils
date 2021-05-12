1ae175d243fe9a5e292ec5375e3d6d5185448074
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-05-12 16:18:42 -0400

    configure process name

--- sam-app/lutil_fan_e2e_producer/app.py
+++ sam-app/lutil_fan_e2e_producer/app.py
@@ -15,17 +15,18 @@ def lambda_handler(event, context):
     print(f"Started at {datetime.now()}")
     iterations = event.get("iterations", 20)
     max_delay = event.get("max_delay", 180)
+    process_name = event.get("process_name", "e2e tests")
 
     event_source = os.environ.get("AWS_LAMBDA_FUNCTION_NAME", "lambda_producer")
     publisher = FanEventPublisher(event_source, os.environ["HANDLER_SNS_TOPIC_ARN"])
     process_id = publisher.generate_process_id()
 
-    # Create a group of fan out events for the "e2e test" process
+    # Create a group of fan out events for the "e2e tests" process
     for i in range(iterations):
         value = i * 33
         task = TaskRecord(
             process_id=process_id,
-            process_name="e2e tests",
+            process_name=process_name,
             task_name=f"task-{i}",
             task_message={"max_delay": max_delay},
         )
