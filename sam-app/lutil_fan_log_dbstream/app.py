f60ea5f147cd3b2ddb6c4524de628abd3ef50546
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-05-12 16:39:16 -0400

    new delta format

--- sam-app/lutil_fan_log_dbstream/app.py
+++ sam-app/lutil_fan_log_dbstream/app.py
@@ -23,20 +23,28 @@ def lambda_handler(event, context):
     for record in event["Records"]:
         old_image = record["dynamodb"].get("OldImage", None)
         if old_image != None:
-            line = "old "
+            # line = "old "
             for k, dynamodb_v in old_image.items():
                 value = list(dynamodb_v.values())[0]
                 field = f" {value:<50}"
-                line = line + field
-            print(line)
+            # print(line)
         new_image = record["dynamodb"].get("NewImage", None)
         if new_image != None:
-            line = "new "
+            pks = (
+                list(old_image["pk"].values())[0]
+                + " / "
+                + list(old_image["sk"].values())[0]
+            )
+            print(pks)
             for k, dynamodb_v in new_image.items():
-                value = list(dynamodb_v.values())[0]
-                field = f" {value:<50}"
-                line = line + field
-            print(line)
+                new_value = list(dynamodb_v.values())[0]
+                old_value = ""
+                if old_image != None:
+                    old_value = list(old_image[k].values())[0]
+
+                if old_value != new_value:
+                    field = f"   {k}: {old_value} -> {new_value}"
+                    print(field)
         print("\n")
 
     return {}
