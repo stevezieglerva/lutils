7dba31934bf4c8e55a4de970a97225aac1ea4c0f
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-05-17 22:00:59 -0400

    update progress script

--- sam-app/tests/integration/check_progress.py
+++ sam-app/tests/integration/check_progress.py
@@ -5,27 +5,24 @@ import sys
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-parentdir = os.path.dirname(parentdir) + "/common_layer/python"
+parentdir = os.path.dirname(parentdir) + "/common_layer_hex/python"
 sys.path.insert(0, parentdir)
 print("Updated path:")
 print(json.dumps(sys.path, indent=3))
 
 from DynamoDB import DynamoDB
 
-process_id = sys.argv[1]
-print(f"Getting data for: {process_id}")
-
 
 db = DynamoDB("lutils2-FanProcessingPartTestTable-Q3PVEB6MO2AJ")
 
 
-tasks = db.query_table_begins({"pk": process_id, "sk": "TASK"})
-for task in tasks:
-    task_name = task["task_name"]
-    status = task["status"]
-    print(f"{task_name:<20} - {status}")
-
-
-main_data = db.get_item({"pk": process_id, "sk": process_id})
-progress = main_data["progress"]
-print(f"\n\nProgress: {progress}\n\n")
+processes = db.query_index_begins(
+    "gs1", {"gs1_pk": "STATUS#in_progress", "gs1_sk": "-"}
+)
+print()
+for process in processes:
+    name = process["process_name"]
+    gs1_pk = process["gs1_pk"]
+    progress = process["progress"]
+    print(f"{name:<30} {gs1_pk:<15} {progress}")
+print()
