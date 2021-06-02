7f420d62e611e98b83937e649cc53dc3e4959d8a
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-06-02 17:41:27 -0400

    remove old common layer since replaced with common_hex

--- sam-app/common_layer/python/FanEventOptions.py
+++
@@ -1,7 +0,0 @@
-TASK_CREATED = "task_created"
-TASK_COMPLETED = "task_completed"
-TASK_STARTED = "task_started"
-TASK_COMPLETED = "task_completed"
-TASK_ERROR = "task_error"
-
-PROCESS_COMPLETED = "process_completed"
--- sam-app/common_layer/python/FanTaskStatus.py
+++
@@ -1,4 +0,0 @@
-TASK_CREATED = "created"
-TASK_IN_PROGRESS = "in_progress"
-TASK_ERROR = "error"
-TASK_COMPLETED = "completed"
